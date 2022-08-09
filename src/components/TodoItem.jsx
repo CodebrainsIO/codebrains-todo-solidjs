2d2ffc7a54f7f6bbf6902df355ec9e533d2fe422
CodebrainsIO <coonce.james@codebrains.io>
2022-08-08 23:41:34 -0400

    Add icons

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -6,8 +6,10 @@ import {
   Tr,
   Th,
   Td,
+  IconButton
 } from "@hope-ui/solid";
 import { createSignal, createEffect, onMount, For } from "solid-js";
+import { IoTrashBinSharp } from 'solid-icons/io'
 
 const TodoItem = ({ todo, toggleTodo, removeTodo }) => {
   return (
@@ -16,7 +18,7 @@ const TodoItem = ({ todo, toggleTodo, removeTodo }) => {
       <Td>
         <Checkbox defaultChecked={todo.completed} colorScheme="success" />
       </Td>
-      <Td>25.4</Td>
+      <Td><IconButton aria-label="Search" icon={<IoTrashBinSharp size={24} color="#000000"/>} /></Td>
     </Tr>
   );
 };
